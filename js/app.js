6c090936e44d6a58a14acc54d9160b2af6b1460a
Matthewmr <37819153+Matthewmr@users.noreply.github.com>
2018-04-15 21:29:06 -0700

    Quick Java on contact page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+
+var greeting;
+var today = new Date();
+var hourNow = today.getHours();
+
+document.getElementById("time").innerHTML = "I am most likely sleeping or preparing for work at this time";
